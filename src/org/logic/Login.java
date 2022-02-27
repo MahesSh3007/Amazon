581e8d9ea215605390944b7ba32b95a586700b13
Mahesh S <MahesSh3007>
2022-02-27 21:29:35 +0530

    Password Functionality

--- src/org/logic/Login.java
+++ src/org/logic/Login.java
@@ -1,5 +1,11 @@
 package org.logic;
 
 public class Login {
-
+	
+	public static void passWord() {
+		System.out.println("Password is: 12345");
+	}
+	public static void main(String[] args) {
+		passWord();
+	}
 }
