dfdf1ff97a40dbf4f274626661a97108305b9530
Mahesh S <MahesSh3007>
2022-02-27 21:17:55 +0530

    Username Functionality

--- src/org/logic/Login.java
+++ src/org/logic/Login.java
@@ -1,5 +1,11 @@
 package org.logic;
 
 public class Login {
-
+	
+	public static void userName() {
+		System.out.println("User Name: Mahesh");
+	}
+public static void main(String[] args) {
+	userName();
+}
 }
